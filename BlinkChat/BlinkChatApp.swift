cb0916b40af839568c34c570031af989a6235c14
zainashraf1562 <zainashraf1562@gmail.com>
2024-01-18 17:46:40 -0500

    Initial Commit

---
+++ BlinkChat/BlinkChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkChatApp.swift
+//  BlinkChat
+//
+//  Created by Noman Ashraf on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BlinkChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
